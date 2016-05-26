18448fa56b91e377c71e9a0386c23e944be29392
Alex <Alex@bc_arein>
2016-05-26 15:08:03 +0200

    Initial commit

--- oposso/admin/navigation.php
+++
@@ -1,9 +0,0 @@
-
-<ul>
-	<li><a href="content/type.php">Subscription Type</a></li>
-	<li><a href="content/key.php">Subscription Key</a></li>
-	<li><a href="content/subscription.php">Subscription</a></li>
-	<li><a href="content/user.php">User</a></li>
-	<li><a href="content/customisation.php">Customisation</a></li>
-	<li class="reload"><a class="button_refresh" onclick="reloadTab()">Reload</a></li>
-</ul>
--- oposso/layout/template/how_to.tpl
+++
@@ -1,6 +0,0 @@
-{extends file="layout/template/template.tpl"}
-
-{block name=content}
-	<h1>{$LOCAL.howto.head}</h1>
-	<p>{$LOCAL.howto.text}</p>
-{/block}
