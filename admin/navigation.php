5577cad75840dbf8f54ed6d6bea2de60a878576e
Alexander Rein <0bitus@users.noreply.github.com>
2019-10-17 15:45:21 +0200

    Improove navigation

--- admin/navigation.php
+++ admin/navigation.php
@@ -1,9 +1,9 @@
 
 <ul>
-	<li><a href="content/type.php">Subscription Type</a></li>
-	<li><a href="content/key.php">Subscription Key</a></li>
-	<li><a href="content/subscription.php">Subscription</a></li>
-	<li><a href="content/user.php">User</a></li>
-	<li><a href="content/customisation.php">Customisation</a></li>
+	<li><a class="nav-subscription-type" href="content/type.php">Subscription Type</a></li>
+	<li><a class="nav-subscription-key" href="content/key.php">Subscription Key</a></li>
+	<li><a class="nav-subscription" href="content/subscription.php">Subscription</a></li>
+	<li><a class="nav-user" href="content/user.php">User</a></li>
+	<li><a class="nav-customisation" href="content/customisation.php">Customisation</a></li>
 	<li class="reload"><a class="button_refresh" onclick="reloadTab()">Reload</a></li>
 </ul>
